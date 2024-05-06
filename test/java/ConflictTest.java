e9eb008d555577213b3cf13135952a8577ab4cf0
olly-pat <pakolhapak@gmail.com>
2024-05-05 22:22:38 -0500

    changed test

--- test/java/ConflictTest.java
+++ test/java/ConflictTest.java
@@ -6,12 +6,12 @@ public class ConflictTest {
     public void test1(){
         int a = 1;
         int b = 5;
-        System.out.println(a+b);
+        System.out.println(a+b+a+a);
     }
     @Test
     public void test2(){
         String m = "blabla";
-        Assertions.assertTrue(m.equals("blabla"));
+        Assertions.assertTrue(m.equals("blab"));
 
     }
 
